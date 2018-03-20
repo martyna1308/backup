c1e9aac89653965a5deaabdeb465867d190984a0
Martyna Banaś <martyna1308@gmail.com>
2018-03-20 22:22:54 +0100

    responsive

---
+++ trening/1/js/main.js
@@ -0,0 +1,25 @@
+var menu = document.querySelector("#menu ul");
+var button = document.querySelector("#menu button");
+var menuIcon1 = document.querySelector(".fa-bars");
+var menuIcon2 = document.querySelector(".fa-times");
+var circle = document.querySelector("#relax-grey img");
+
+menuIcon1.addEventListener("click", function () {
+    menu.style.display = "flex";
+    menu.style.right = 0; 
+    
+    menuIcon1.style.visibility = "hidden";
+    menuIcon2.style.visibility = "visible";
+})
+
+menuIcon2.addEventListener("click", function () {
+    menu.style.right = "-100%";
+    menuIcon2.style.visibility = "hidden";
+    menuIcon1.style.visibility = "visible";
+    menu.style.display = "none";
+})
+
+
+
+
+
