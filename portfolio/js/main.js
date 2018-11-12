48fbb92600b46e0696fa817952c86ff7a29747ce
Martyna Banaś <martyna1308@gmail.com>
2018-11-12 19:05:00 +0100

    3

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,45 +1,58 @@
-$(document).ready(function(){
- 
-  $("a").on('click', function(event) {
+$(document).ready(function () {
 
-    
-    if (this.hash !== "") {
-  
-      event.preventDefault();
-
-      var hash = this.hash;
+    $("a").on('click', function (event) {
 
 
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 600, function(){
-   
+        if (this.hash !== "") {
 
-        window.location.hash = hash;
-      });
-    } 
-  });
-    
-    let heightView = $(window).height() /2;
-    
-    let positionHeader = $("header").offset().top;
-    let positionAbout = $("#about").offset().top;
-    let positionPortfolio = $("#portfolio").offset().top; 
-    let positionContact = $("#contact").offset().top;
+            event.preventDefault();
 
-    
-    $(window).scroll(function(){
-        if($(this).scrollTop() > positionHeader+heightView && $(this).scrollTop() < positionHeader+(heightView*2)  ) {
+            var hash = this.hash;
+
+
             $('html, body').animate({
-        scrollTop: positionHeader
-      }, 600);
-       }
+                scrollTop: $(hash).offset().top
+            }, 600, function () {
+
+
+                window.location.hash = hash;
+            });
+        }
+    });
+
+    //    let heightView = $(window).height() /2;
+    //    
+    //    let positionHeader = $("header").offset().top;
+    //    let positionAbout = $("#about").offset().top;
+    //    let positionPortfolio = $("#portfolio").offset().top; 
+    //    let positionContact = $("#contact").offset().top;
+    //
+    //    
+    //    $(window).scroll(function(event){
+    //        if($(this).scrollTop() > positionHeader+heightView && $(this).scrollTop() < positionHeader+(heightView*2)  ) {
+    //            $('html, body').animate({
+    //        scrollTop: positionAbout
+    //      }, 600);
+    //       }
+    //        event.stopPropagation();
+    //    })
+    //
+
+/*---------SCROLL UP BUTTON-----------*/
+    $(window).scroll(function (event) {
+        let positionPortfolio = $("#portfolio").offset().top;
+        if ($(this).scrollTop() > positionPortfolio) {
+            $(".up-button i").css("display", "inline-block");
+        } else {
+            $(".up-button i").css("display", "none");
+        }
+    });
+    
+    
+    $(".up-button i").click(function() {
+        $("html, body").animate({ scrollTop: 0 }, "slow");
     })
 
-    
-   
+/*---------MENU-----------------------*/
 
-    
 });
-
-    
