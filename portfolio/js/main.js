972357f3bc685caf0fae6cac09605844608208d2
Martyna Banaś <martyna1308@gmail.com>
2019-01-06 16:24:45 +0100

    2019-01-06

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -14,45 +14,43 @@ $(document).ready(function () {
                 scrollTop: $(hash).offset().top
             }, 600, function () {
 
-
-                window.location.hash = hash;
             });
         }
     });
 
-    //    let heightView = $(window).height() /2;
-    //    
-    //    let positionHeader = $("header").offset().top;
-    //    let positionAbout = $("#about").offset().top;
-    //    let positionPortfolio = $("#portfolio").offset().top; 
-    //    let positionContact = $("#contact").offset().top;
-    //
-    //    
-    //    $(window).scroll(function(event){
-    //        if($(this).scrollTop() > positionHeader+heightView && $(this).scrollTop() < positionHeader+(heightView*2)  ) {
-    //            $('html, body').animate({
-    //        scrollTop: positionAbout
-    //      }, 600);
-    //       }
-    //        event.stopPropagation();
-    //    })
-    //
-
-/*---------SCROLL UP BUTTON-----------*/
-    $(window).scroll(function (event) {
-        let positionPortfolio = $("#portfolio").offset().top;
-        if ($(this).scrollTop() > positionPortfolio) {
-            $(".up-button i").css("display", "inline-block");
-        } else {
-            $(".up-button i").css("display", "none");
-        }
+    $(function () {
+        $.scrollify({
+            section: ".scrollSection",
+        });
     });
-    
-    
-    $(".up-button i").click(function() {
-        $("html, body").animate({ scrollTop: 0 }, "slow");
+
+    $.scrollify({
+        section: "section",
+        sectionName: "scrollSection",
+        interstitialSection: "",
+        easing: "easeOutExpo",
+        scrollSpeed: 800,
+        offset: 0,
+        scrollbars: true,
+        standardScrollElements: "",
+        setHeights: true,
+        overflowScroll: true,
+        updateHash: true,
+        touchScroll: true,
+        before: function () {},
+        after: function () {},
+        afterResize: function () {},
+        afterRender: function () {}
+    });
+
+
+    $("#menu button").click(function() {
+        console.log('dziala');
+        $("nav").css("margin-left: 0");
     })
 
-/*---------MENU-----------------------*/
+
+
+
 
 });
