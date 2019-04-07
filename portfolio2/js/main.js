abc8a46f7ab318bcd86245be3d722a87b1fe9eb9
martyna1308 <martyna1308@gmail.com>
2019-04-07 17:22:27 +0200

    responsive

--- portfolio2/js/main.js
+++ portfolio2/js/main.js
@@ -1,19 +1,40 @@
-$(document).ready(function() {
-    
-  $("html").on("submit","#contact_form",function(e){
-    
-    e.preventDefault();
-    
-    $("#send_form_status").html('').hide();
-    
-      var data=$("#contact_form").serialize();
-      
-    $.post("../send_form.php",data,function(res){
-      $("#send_form_status").html(res.msg).show();
-      if(res.status==1){ 
-        $("#contact_form")[0].reset();
-      } 
-    });
-  });
-});
+$(document).ready(function () {
 
+    $("html").on("submit", "#contact_form", function (e) {
+
+        e.preventDefault();
+
+        $("#send_form_status").html('').hide();
+
+        var data = $("#contact_form").serialize();
+
+        $.post("../send_form.php", data, function (res) {
+            $("#send_form_status").html(res.msg).show();
+            if (res.status == 1) {
+                $("#contact_form")[0].reset();
+            }
+        });
+    });
+
+
+
+    $(".menu-button").click(function () {
+        document.getElementsByTagName("nav")[0].classList.toggle("open-menu");
+
+    });
+
+    if (document.getElementsByClassName("portfolio-box").length <= 6) {
+        document.getElementsByClassName("portfolio-more")[0].style.display = "none";
+        
+        document.getElementById("contact").ctyle.marginTop = 20 + "px"
+    }
+
+
+
+
+
+
+
+
+
+});
