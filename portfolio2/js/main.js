c2140929b2f603632846d525006d9e190f31858d
Martyna Banaś <martyna1308@gmail.com>
2019-05-09 16:09:21 +0200

    2019-05-08

--- portfolio2/js/main.js
+++ portfolio2/js/main.js
@@ -1,22 +1,5 @@
 $(document).ready(function () {
 
-    $("html").on("submit", "#contact_form", function (e) {
-
-        e.preventDefault();
-
-        $("#send_form_status").html('').hide();
-
-        var data = $("#contact_form").serialize();
-
-        $.post("../send_form.php", data, function (res) {
-            $("#send_form_status").html(res.msg).show();
-            if (res.status == 1) {
-                $("#contact_form")[0].reset();
-            }
-        });
-    });
-
-
 
     $(".menu-button").click(function () {
         document.getElementsByTagName("nav")[0].classList.toggle("open-menu");
@@ -30,8 +13,13 @@ $(document).ready(function () {
     }
 
 
-
-
+    $(".portfolio-box").click(function() {
+       document.getElementsByClassName("container-portfolio")[0].classList.toggle("portfolio-clicked");
+        document.body.classList.toggle("gallery-overflow");
+        
+        
+    })
+    
 
 
 });
