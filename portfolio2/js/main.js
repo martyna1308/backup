bf85cdba53b8e9682c5d1f8a6921100b42bf229a
Martyna Banaś <martyna1308@gmail.com>
2019-05-09 21:19:58 +0200

    elo

--- portfolio2/js/main.js
+++ portfolio2/js/main.js
@@ -9,17 +9,27 @@ $(document).ready(function () {
 
     if (document.getElementsByClassName("portfolio-box").length <= 6) {
         document.getElementsByClassName("portfolio-more")[0].style.display = "none";
-        
+
     }
 
 
-    $(".portfolio-box").click(function() {
-       document.getElementsByClassName("container-portfolio")[0].classList.toggle("portfolio-clicked");
+    $(".portfolio-box").click(function (e) {
+        document.getElementsByClassName("container-portfolio")[0].classList.toggle("portfolio-clicked");
         document.body.classList.toggle("gallery-overflow");
-        
-        
+        let targetsMasks = document.getElementsByClassName("portfolio-mask");
+        let targetsBoxes = document.getElementsByClassName("portfolio-box");
+
+        for(let j in targetsBoxes) {
+            targetsBoxes[j].style.display = "none";
+        };
+
+        for (let i in targetsMasks) {
+            if (e.target === targetsMasks[i]) {
+                targetsBoxes[i].style.display = "flex";
+            }
+        }
     })
-    
+
 
 
 });
