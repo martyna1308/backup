563265be90349c79763f7df47bf19a1bb8bc3ae9
Martyna Banaś <martyna1308@gmail.com>
2019-08-14 23:17:20 +0200

    2019-08-14

--- prywatne/trening/31/js/main.js
+++ prywatne/trening/31/js/main.js
@@ -1,3 +1,13 @@
 //mobile menu
 
-document.querySelector
+let menuElements = document.getElementsByClassName("main__menuItem");
+let menu = document.getElementsByClassName("main__menuList")[0];
+let menuButton = document.getElementsByClassName("main__bars")[0];
+
+
+
+menuButton.addEventListener("click", () => {
+
+    menu.classList.toggle("main__mobile");
+
+});
